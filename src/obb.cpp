9fbeac889b7cfdc2955c2ed57ad1635fb7361a56
zju-lx <2475271071@qq.com>
2024-01-08 01:00:27 +0800

    Update obb.cpp

--- src/obb.cpp
+++ src/obb.cpp
@@ -310,8 +310,8 @@ int Obb::intersectWith(Obb &other)
     if (t > ra + rb)
         return false;
 
-    other.drawFlag = 200;
-    this->drawFlag = 200;
+    // other.drawFlag = 200;
+    // this->drawFlag = 200;
     // return true;
 
     // std::cout<<"extend: "<<this->extends.x<<" "<<this->extends.y<<" "<<this->extends.z<<std::endl;
